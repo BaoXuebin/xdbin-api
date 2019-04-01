b46142c4daf394324ce6bcd9ce0b1343cdf81518
BaoXuebin <baoxbin@hotmail.com>
2019-04-01 23:08:15 +0800

    update sqsl

--- src/main/java/com/xdbin/blog/BlogSql.java
+++ src/main/java/com/xdbin/blog/BlogSql.java
@@ -10,7 +10,7 @@ public class BlogSql {
         String sql = "SELECT b.blog_id, b.title, b.summary, GROUP_CONCAT(bt.tag_id SEPARATOR '|') AS tags, b.publish_time, b.update_time, b.if_pub as pub " +
                 "FROM t_blog b LEFT JOIN t_blog_tag bt ON b.blog_id = bt.blog_id WHERE 1 = 1 ";
         sql += where(params);
-        sql += " GROUP BY b.blog_id ORDER BY b.update_time DESC ";
+        sql += " GROUP BY b.blog_id ORDER BY b.publish_time DESC ";
         return sql;
     }
 
@@ -21,8 +21,8 @@ public class BlogSql {
         return sql;
     }
 
-    public static final String GROUP_BY_MONTHLY = "SELECT DATE_FORMAT(update_time, '%Y-%m') as month, COUNT(1) as count FROM t_blog WHERE if_pub = 1 " +
-            "GROUP BY DATE_FORMAT(update_time, '%Y-%m') ORDER BY month DESC";
+    public static final String GROUP_BY_MONTHLY = "SELECT DATE_FORMAT(publish_time, '%Y-%m') as month, COUNT(1) as count FROM t_blog WHERE if_pub = 1 " +
+            "GROUP BY DATE_FORMAT(publish_time, '%Y-%m') ORDER BY month DESC";
 
     private static String where(Map<String, Object> params) {
         if (StringUtils.isEmpty(params)) return "";
@@ -34,7 +34,7 @@ public class BlogSql {
             whereSql += " AND b.title like :title ";
         }
         if (!StringUtils.isEmpty(params.get("month"))) {
-            whereSql += " AND date_format(update_time, '%Y-%m') = :month ";
+            whereSql += " AND date_format(publish_time, '%Y-%m') = :month ";
         }
         if (!StringUtils.isEmpty(params.get("tagId"))) {
             whereSql += " AND b.blog_id in (SELECT blog_id FROM t_blog_tag WHERE tag_id = :tagId) ";
